cce8e1927b2fa580ccb9963a023c7722cedc8788
timamtalukdar <timamtalukdar@gmail.com>
2017-11-18 00:14:12 +0600

    Student+Teacher Login+Signup URL ADDED

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^student_login/$', views.student_login, name='student_loginn'),
+    url(r'^student_signup/$', views.student_signup, name='student_signup'),
+
+    url(r'^teacher_login/$', views.teacher_login, name='teacher_login'),
+    url(r'^teacher_signup/$', views.teacher_signup, name='teacher_signup'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,18 @@ from .models import Person, Assignment, StudentAssignment
 
 def index(request):
     return render(request, 'index.html',)
+
+
+#Student Section
+def student_login(request):
+    return render(request, 's-log-in.html',)
+
+def student_signup(request):
+    return render(request, 's-sign-up.html',)
+
+#Teacher Section
+def teacher_login(request):
+    return render(request, 't-log-in.html',)
+
+def teacher_signup(request):
+    return render(request, 't-sign-up.html',)
